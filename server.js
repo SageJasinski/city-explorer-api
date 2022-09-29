b5beaf992a38af5021515bacf7aaab5d5344410c
Sage Jasinski <sagejasinski@gmail.com>
2022-09-28 20:09:55 -0700

    backend api weather added

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const weather = require("./weather.json");
+const axios = require("axios");
+// const {response} = require('express')
 
 // initializes our express server
 const app = express();
@@ -12,8 +14,38 @@ app.use(cors());
 
 //port variable to transfer information on
 let PORT = process.env.PORT || 3002;
+let API = process.env.WEATHER_ACCESS_KEY;
+console.log('api key', API);
 
-app.listen(PORT, () => console.log('listening for connection'))
+app.listen(PORT, () => console.log('listening for connection', PORT));
+
+
+//end points here
+app.get("/weather", grabWeather);
+
+async function grabWeather(req,res) {
+    let searchQuery = req.query.searchQuery; 
+    let destination = `https://api.weatherbit.io/v2.0/forecast/daily?key=${API}&city=${searchQuery}`;
+    
+    try {
+        const respond = await axios.get(destination);
+        const weatherData = respond.data.data.map(value => new Weather(value));
+        res.status(200).send(weatherData);
+    }catch(error) {
+        res.status(500).send(`server side: ${error}`);
+    }
+}
+
+
+class Weather {
+    constructor(weatherObj){
+        this.low_temp = weatherObj.low_temp;
+        this.high_temp = weatherObj.high_temp;
+        this.description = weatherObj.weather.description;
+    }
+}
+
+
 
 app.get("/seattle", (req, res) => {
     res.send(weather[0]);
